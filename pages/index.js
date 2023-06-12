5b1a4333e7285c497bfd81de99eb62556b5a783b
Duddy5683 <126365878+Duddy5683@users.noreply.github.com>
2023-06-12 06:18:23 +0000

    changed lyrics to fantasy

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import { useState } from 'react';
 
 const Home = () => {
   const [userInput, setUserInput] = useState('');
-  const [lyrics, setLyrics] = useState([]);
+  const [fantasy, setFantasy] = useState([]);
   const [currentLine, setCurrentLine] = useState('');
   const [isGenerating, setIsGenerating] = useState(false);
   const [playing, setIsPlaying] = useState(false);
